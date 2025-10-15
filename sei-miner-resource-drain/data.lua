ea87c3ad064b681b217fb9c733a2a3da0cb6ef03
xkyouchoux <xkyouchoux@hotmail.com>
2025-10-14 22:31:41 -0400

    sei-miner-resource-drain updated

--- sei-miner-resource-drain/data.lua
+++
@@ -1,5 +0,0 @@
-data.raw["mining-drill"]["electric-mining-drill"].resource_drain_rate_percent = settings.startup["sei-electric-mining-drill-resource-drain-rate"].value
-data.raw["mining-drill"]["area-mining-drill"].resource_drain_rate_percent = settings.startup["sei-area-mining-drill-resource-drain-rate"].value
-
-require("prototypes.Krastorio2")
-require("prototypes.um-standalone-big-mining-drill")
---
+++ sei-miner-resource-drain/prototypes/aai-industry.lua
@@ -0,0 +1,3 @@
+if not mods["aai-industry"] then return end
+
+data.raw["mining-drill"]["area-mining-drill"].resource_drain_rate_percent = settings.startup["sei-area-mining-drill-resource-drain-rate"].value
