14f762a6672a59cc400951f915458b875ef7dd24
xkyouchoux <xkyouchoux@hotmail.com>
2025-10-14 22:22:15 -0400

    ...

--- sei-library/data.lua
+++ sei-library/data.lua
@@ -1,3 +1,10 @@
 require("prototypes.particles")
 require("prototypes.smoke")
 require("prototypes.entity.circuit-network")
+
+-- Required to load space-travel feature flag without space age
+if not data.raw.tile["empty-space"] then
+  local empty_space = table.deepcopy(data.raw.tile["out-of-map"])
+  empty_space.name = "empty-space"
+  data:extend{empty_space}
+end
