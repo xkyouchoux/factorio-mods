728314e1208c392d03dbb9cfdbbd5dcc0297c502
xkyouchoux <xkyouchoux@hotmail.com>
2025-10-17 14:18:50 -0400

    sei-agriculture-tower:1.0.4

--- sei-agricultural-tower/data.lua
+++ sei-agricultural-tower/data.lua
@@ -5,3 +5,5 @@ require("prototypes.entity.tree_plant")
 require("prototypes.item")
 require("prototypes.recipe")
 require("prototypes.technology")
+
+se_delivery_cannon_recipes["tree-seed"] = {name="tree-seed"}
