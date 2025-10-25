1d48cf472dd722ae51f653651ea1521e3166fdf8
xkyouchoux <xkyouchoux@hotmail.com>
2025-10-24 21:18:48 -0400

    sei-captive-biters:0.1.10

--- sei-captive-biters/data-updates.lua
+++
@@ -1,4 +0,0 @@
-local data_util = require("__sei-library__.data_util")
-
-data_util.replace_or_add_ingredient("se-capsule-small-biter", nil, "biter-egg", 1)
-data_util.replace_or_add_ingredient("se-capsule-small-spitter", nil, "biter-egg", 1)
