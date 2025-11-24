5fccd11a5e0c4f6c4315be547b2ea8971459c657
xkyouchoux <xkyouchoux@hotmail.com>
2025-11-24 16:53:16 -0500

    sei-cryogenic-plant:1.0.10

--- sei-cryogenic-plant/migrations/sei-cryogenic-plant.1.0.1.lua
+++
@@ -1,5 +0,0 @@
-for _,force in pairs(game.forces) do
-    if force.technologies["se-processing-cryonite"].researched then
-        force.technologies["cryogenic-plant"].researched = true
-    end
-end
