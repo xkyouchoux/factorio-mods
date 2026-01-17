1d34a6adbe30afce02e7cc7ef0bfc5b650fa1ebf
xkyouchoux <xkyouchoux@hotmail.com>
2026-01-17 11:50:18 -0500

    sei-stack-inserters:1.0.8

--- sei-stack-inserters/prototypes/Krastorio2-updates.lua
+++ sei-stack-inserters/prototypes/Krastorio2-updates.lua
@@ -2,13 +2,17 @@ if not mods["Krastorio2"] then return end
 
 local data_util = require("__sei-library__.data_util")
 
-if settings.startup["sei-add-stacking-to-kr-loaders"].value then
+if settings.startup["sei-add-stacking-to-kr-loaders"].value and settings.startup["kr-loaders"].value then
     for k,v in pairs(data.raw["loader-1x1"]) do
         if k:find("kr-") == 1 then
             v.max_belt_stack_size = data.raw["inserter"]["stack-inserter"].max_belt_stack_size
             v.adjustable_belt_stack_size = true
             v.wait_for_full_stack = settings.startup["sei-loaders-wait-for-full-stack"].value
         end
+    end
+
+    if settings.startup["sei-loaders-wait-for-full-stack"].value then
+        data.raw["utility-constants"]["default"].minimum_recipe_overload_multiplier = data.raw["utility-constants"]["default"].max_belt_stack_size
     end
 end
 
